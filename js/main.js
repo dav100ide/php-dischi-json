512d161b557730cf75fd9d2b1b50eb692cf45649
dav100ide <oreglio.dev@gmail.com>
2022-12-16 18:20:09 +0100

    aggiunge select => filtraPer genere al frontend :(

--- js/main.js
+++ js/main.js
@@ -4,13 +4,29 @@ createApp({
    data() {
       return {
          cds: [],
+         filteredCds: [],
+         currentGenre: 'default',
       };
+   },
+   methods: {
+      changeGenre() {
+         this.filteredCds = [];
+         this.cds.forEach((cd) => {
+            if (cd.genre === this.currentGenre) {
+               this.filteredCds.push(cd);
+            }
+         });
+         if (this.currentGenre === 'default') {
+            this.filteredCds = this.cds;
+         }
+      },
    },
    created() {
       axios
          .get('http://localhost/DICEMBRE/php-dischi-json/api.php') //
          .then((response) => {
             this.cds = response.data;
+            this.filteredCds = this.cds;
          });
    },
 }).mount('#app');
